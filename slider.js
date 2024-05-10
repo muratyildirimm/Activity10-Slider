b3ee4ee98c1fcd5b16db4327f14089db9e3825e0
Murat Yıldırım <109426253+muratyildirimm@users.noreply.github.com>
2024-05-10 21:27:36 +0300

    Add files via upload

---
+++ slider.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+  $("#slider").bxSlider({
+    auto: true,
+    slideWidth: 250,
+    slideMargin: 10,
+    randomStart: true,
+    speed: 300,
+    moveSlides: 1,
+    pager: true,
+    pagerType: "short",
+    touchEnabled: false,
+    captions: true,
+  });
+});
